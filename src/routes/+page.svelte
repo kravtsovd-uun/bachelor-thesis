4a01e732e23a6cea9011ea6133cb55acbfa1e5f2
Dmytro Kravtsov <dmytro.kravtsov@unicorn.com>
2024-03-26 10:40:30 +0100

    Logout mechanism complete. Auth session keep alive complete.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,4 +2,13 @@
 	export let data;
 </script>
 
-<h1 class="h1">Hello {data.user.name || 'user'}!</h1>
+<h1 class="h1">Hello {data?.user?.name || 'user'}!</h1>
+{#if data?.user}
+	<form method="POST" action="/logout">
+		<button type="submit" class="variant-outline-primary btn hover:variant-filled-primary"
+			>Logout</button
+		>
+	</form>
+{:else}
+	<a href="/login" class="variant-filled-primary btn">Login</a>
+{/if}
--- src/routes/login/+page.server.js
+++ src/routes/login/+page.server.js
@@ -1,5 +1,13 @@
 import { redirect } from '@sveltejs/kit';
 
+export async function load({ locals }) {
+	if (locals.pb.authStore.isValid) {
+		throw redirect(303, '/');
+	}
+
+	return {};
+}
+
 export const actions = {
 	login: async ({ locals, request }) => {
 		const formData = await request.formData();
